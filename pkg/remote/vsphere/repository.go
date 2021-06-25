d1230a5b22e1754ce1e647a27338705c22f5ef53
William Beuil <william.beuil@gmail.com>
2021-06-25 16:14:20 +0200

    Fix uuid retriever

--- pkg/remote/vsphere/repository.go
+++ pkg/remote/vsphere/repository.go
@@ -6,6 +6,9 @@ import (
 	"github.com/cloudskiff/driftctl/pkg/remote/cache"
 	"github.com/vmware/govmomi"
 	"github.com/vmware/govmomi/find"
+	"github.com/vmware/govmomi/property"
+	"github.com/vmware/govmomi/vim25/mo"
+	"github.com/vmware/govmomi/vim25/types"
 )
 
 type VSphereRepository interface {
@@ -42,20 +45,30 @@ func NewVSphereRepository(config vsphereConfig, c cache.Cache) (*vsphereReposito
 }
 
 func (r *vsphereRepository) ListVirtualMachines() ([]string, error) {
-	ctx2 := context.TODO()
+	ctx := context.TODO()
 	finder := find.NewFinder(r.client.Client, true)
-	dc, err := finder.DefaultDatacenter(ctx2)
+	dc, err := finder.DefaultDatacenter(ctx)
 	if err != nil {
 		return nil, err
 	}
 	finder.SetDatacenter(dc)
-	vms, err := finder.VirtualMachineList(ctx2, "*")
+	vms, err := finder.VirtualMachineList(ctx, "*")
+	if err != nil {
+		return nil, err
+	}
+	collector := property.DefaultCollector(r.client.Client)
+	var refs []types.ManagedObjectReference
+	for _, vm := range vms {
+		refs = append(refs, vm.Reference())
+	}
+	var vmt []mo.VirtualMachine
+	err = collector.Retrieve(ctx, refs, []string{"config.uuid"}, &vmt)
 	if err != nil {
 		return nil, err
 	}
 	var ids []string
-	for _, vm := range vms {
-		ids = append(ids, vm.InventoryPath)
+	for _, vm := range vmt {
+		ids = append(ids, vm.Config.Uuid)
 	}
 	return ids, nil
 }
